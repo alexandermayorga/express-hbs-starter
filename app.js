837de7cefe676359c912216d0a46f4fdb0edca6c
3MenMovers-Owner <ninjas@3menmovers.com>
2020-06-11 11:13:29 -0500

    setup handlebars as view engine and initial setup of hbs templates

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const hbs = require('express-handlebars'); //Templating Engine
+const xhbs = require('express-handlebars'); //Templating Engine
 const mongoose = require('mongoose'); //Mongoose - DB util
 
 var indexRouter = require('./routes/index');
@@ -16,7 +17,43 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//// ############# HBS SETUP ############# ////
+app.engine('hbs', xhbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+//// ############# HBS SETUP ############# ////
+
+
+// ROUTES
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+
+    const errors = {
+        status:  err.status,
+        stack: err.stack
+    };
+
+    res.locals.error = req.app.get('env') === 'development' ? errors : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const data = { 
+    title: 'Index Page',
+    content: "This is a post"
+   }
+  res.render('index', data);
 });
 
 module.exports = router;
